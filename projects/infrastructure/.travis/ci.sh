114a334b1d4452c2d335bedde4b31815844256c6
Ricool06 <ricoolohsix@hotmail.com>
2019-02-16 12:30:22 +0000

    Install terraform with sudo

--- projects/infrastructure/.travis/ci.sh
+++ projects/infrastructure/.travis/ci.sh
@@ -1,6 +1,6 @@
 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
 unzip terraform_0.11.11_linux_amd64.zip
-mv terraform /usr/local/bin
+sudo mv terraform /usr/local/bin
 rm terraform && rm terraform_0.11.11_linux_amd64.zip
 terraform init
 terraform validate deploy-system
