1ae888df31d3cf7a7549afd2aedb34ee8a57303f
Ricool06 <ricoolohsix@hotmail.com>
2019-02-16 12:37:37 +0000

    Allow terraform to work without tfvars file

--- projects/infrastructure/providers.tf
+++ projects/infrastructure/providers.tf
@@ -1,5 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
 variable "aws_region" {
   default = "eu-west-1"
 }
@@ -9,7 +7,5 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
   region  = "${var.aws_region}"
 }
