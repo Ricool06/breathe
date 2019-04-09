3ab8b87c0271644a1d09e019e5c8609f9acf41bf
Ricool06 <ricoolohsix@hotmail.com>
2019-04-09 19:23:22 +0100

    Create week slider component

---
+++ projects/web-ui/src/app/shared/week-slider/week-slider.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import * as moment from 'moment';
+import { MomentRange } from 'src/app/model';
+import { MatSliderChange } from '@angular/material';
+
+@Component({
+  selector: 'app-week-slider',
+  templateUrl: './week-slider.component.html',
+  styleUrls: ['./week-slider.component.scss'],
+})
+export class WeekSliderComponent implements OnInit {
+
+  @Output() dateRange = new EventEmitter<MomentRange>();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.emitMomentRange(0);
+  }
+
+  changeDateRange(sliderChange: MatSliderChange) {
+    const { value } = sliderChange;
+
+    this.emitMomentRange(value);
+  }
+
+  private emitMomentRange(weeksAgo: number) {
+    const dateTo = moment().subtract(weeksAgo, 'weeks');
+    const dateFrom = dateTo.clone().subtract(7, 'days');
+
+    this.dateRange.emit({dateFrom, dateTo});
+  }
+}
