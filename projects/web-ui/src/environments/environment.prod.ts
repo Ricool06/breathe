f9a2a7555214a4ce11e8ca245ffb63359114dcfd
Ricool06 <ricoolohsix@hotmail.com>
2019-05-16 06:22:23 +0100

    Point web-ui at pred api

--- projects/web-ui/src/environments/environment.prod.ts
+++ projects/web-ui/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     rootPath: '/v1',
   },
   predictionApi: {
-    host: '',
+    host: 'http://ec2-34-253-216-253.eu-west-1.compute.amazonaws.com',
     port: 5890,
     rootPath: '',
   },
